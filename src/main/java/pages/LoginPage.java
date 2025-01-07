5ecd4fffe21dcbda5c988a0479a9a128ea14d451
Recep ALICI <alicirecepp@gmail.com>
2025-01-07 20:24:10 +0100

    07/01/2025/V2

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,9 +3,9 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage {
+
 	@FindBy(id = "username")
 	private WebElement usernameInput;
 
@@ -15,15 +15,15 @@ public class LoginPage extends BasePage {
 	@FindBy(id = "loginButton")
 	private WebElement loginButton;
 
+	// Constructor
 	public LoginPage(WebDriver driver) {
 		super(driver);
-		PageFactory.initElements(driver, this);
 	}
 
+	// Login işlemi
 	public void login(String username, String password) {
 		type(usernameInput, username);
 		type(passwordInput, password);
 		click(loginButton);
 	}
 }
-
